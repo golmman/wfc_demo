e19ea64f856416b48f408606f70e91665c50d1ab
Dirk Kretschmann <kretschmanndi@gmail.com>
2023-03-06 23:11:39 +0100

    add term2d, image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
+use term2d::controller::Controller;
+use term2d::model::color::Color;
+use term2d::model::event::Event;
+use term2d::model::key::Key;
+use term2d::model::point::Point;
+use term2d::model::rgba::Rgba;
+use term2d::view::canvas::halfblock::HalfblockCanvas;
+use term2d::view::canvas::Canvas;
+
+struct DotController {
+    frame: u32,
+    canvas: HalfblockCanvas,
+}
+
+impl Controller<HalfblockCanvas> for DotController {
+    fn update(&mut self, event: Event) -> bool {
+        match event {
+            Event::Key(key) => match key {
+                Key::Char('q') => return false,
+                Key::Ctrl('c') => return false,
+                _ => {}
+            },
+            Event::Resize => {}
+            Event::Elapse => {}
+        }
+
+        self.canvas.clear();
+        self.canvas.draw_text(
+            &Point::new(2, 0),
+            &Color {
+                fg: Rgba::white(),
+                bg: Rgba::transparent(),
+            },
+            &format!("press 'q' to quit, frame: {}", self.frame),
+        );
+        self.canvas.draw_pixel(&Point::new(10, 7), &Rgba::red());
+        self.canvas.display();
+
+        self.frame += 1;
+
+        true
+    }
+
+    fn get_canvas(&mut self) -> &mut HalfblockCanvas {
+        &mut self.canvas
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let controller = DotController {
+        frame: 0,
+        canvas: HalfblockCanvas::new(),
+    };
+    term2d::run(controller);
 }
