c4bb83e104343d761daa86b10667250c029ae4dc
Dirk Kretschmann <kretschmanndi@gmail.com>
2023-03-26 18:53:00 +0200

    outsource controller

--- src/controller.rs
+++ src/controller.rs
@@ -1,1 +1,58 @@
+use std::path::Path;
 
+use image::GenericImageView;
+
+use term2d::model::event::Event;
+use term2d::model::image::Image;
+use term2d::model::key::Key;
+use term2d::view::canvas::halfblock::HalfblockCanvas;
+
+use crate::model::state::State;
+use crate::view::renderer::Renderer;
+
+pub struct Controller {
+    renderer: Renderer,
+    state: State,
+}
+
+impl Controller {
+    pub fn new() -> Self {
+        let img_raw = load_image_raw("data/flowers.png");
+        let img = Image::from(img_raw);
+
+        let renderer = Renderer::new();
+        let state = State { frame: 0, img };
+        Self { renderer, state }
+    }
+}
+
+impl term2d::controller::Controller<HalfblockCanvas> for Controller {
+    fn update(&mut self, event: Event) -> bool {
+        match event {
+            Event::Key(key) => match key {
+                Key::Char('q') => return false,
+                Key::Ctrl('c') => return false,
+                _ => {}
+            },
+            Event::Resize => {}
+            Event::Elapse => {}
+        }
+
+        self.renderer.display(&self.state);
+
+        self.state.frame += 1;
+
+        true
+    }
+
+    fn get_canvas(&mut self) -> &mut HalfblockCanvas {
+        &mut self.renderer.canvas
+    }
+}
+
+fn load_image_raw<T: AsRef<Path>>(path: T) -> (u32, u32, Vec<u8>) {
+    let img = image::open(path).unwrap();
+    let (width, height) = img.dimensions();
+    let raw = img.into_bytes();
+    (width, height, raw)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,10 @@
-use std::path::Path;
-
-use image::GenericImageView;
-
-use model::state::State;
-use term2d::model::event::Event;
-use term2d::model::image::Image;
-use term2d::model::key::Key;
-use term2d::view::canvas::halfblock::HalfblockCanvas;
-use view::renderer::Renderer;
+use controller::Controller;
 
 pub mod controller;
 pub mod model;
 pub mod view;
 
-struct Controller {
-    renderer: Renderer,
-    state: State,
-}
-
-impl term2d::controller::Controller<HalfblockCanvas> for Controller {
-    fn update(&mut self, event: Event) -> bool {
-        match event {
-            Event::Key(key) => match key {
-                Key::Char('q') => return false,
-                Key::Ctrl('c') => return false,
-                _ => {}
-            },
-            Event::Resize => {}
-            Event::Elapse => {}
-        }
-
-        self.renderer.display(&self.state);
-
-        self.state.frame += 1;
-
-        true
-    }
-
-    fn get_canvas(&mut self) -> &mut HalfblockCanvas {
-        &mut self.renderer.canvas
-    }
-}
-
-fn load_image_raw<T: AsRef<Path>>(path: T) -> (u32, u32, Vec<u8>) {
-    let img = image::open(path).unwrap();
-    let (width, height) = img.dimensions();
-    let raw = img.into_bytes();
-    (width, height, raw)
-}
-
 fn main() {
-    let img_raw = load_image_raw("data/flowers.png");
-    let img = Image::from(img_raw);
-
-    let renderer = Renderer::new();
-    let state = State { frame: 0, img };
-    let controller = Controller { renderer, state };
+    let controller = Controller::new();
     term2d::run(controller);
 }
