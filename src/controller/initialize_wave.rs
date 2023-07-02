8e9b47bd19e21c36d72c69394e87369ea6f8546d
Dirk Kretschmann <kretschmanndi@gmail.com>
2023-07-02 17:21:09 +0200

    optimize wave initialization

--- src/controller/initialize_wave.rs
+++ src/controller/initialize_wave.rs
@@ -2,7 +2,8 @@ use std::time::Instant;
 
 use log::info;
 
-use crate::model::{pattern_propagator::PatternPropagator, wave::Wave};
+use crate::model::pattern_propagator::PatternPropagator;
+use crate::model::wave::Wave;
 
 pub fn initialize_wave(
     pattern_propagator: &PatternPropagator,
@@ -12,21 +13,19 @@ pub fn initialize_wave(
     info!("initializing wave...");
     let now = Instant::now();
 
-    let mut wave = Wave {
-        width: target_image_width,
-        height: target_image_height,
-        indices: Vec::new(),
-    };
-
-    for i in 0..(wave.width * wave.height) as usize {
-        wave.indices.push(Vec::new());
-        for j in 0..pattern_propagator.pattern_pixels.len() {
-            wave.indices[i].push(j);
-        }
+    let mut pixel_indices = Vec::new();
+    for i in 0..pattern_propagator.pattern_pixels.len() {
+        pixel_indices.push(i);
     }
+    let indices = vec![pixel_indices; (target_image_width * target_image_height) as usize];
 
     info!("  done, took {} ms", now.elapsed().as_millis());
-    info!("  wave width: {}", wave.width);
-    info!("  wave height: {}", wave.height);
-    wave
+    info!("  wave width: {}", target_image_width);
+    info!("  wave height: {}", target_image_height);
+
+    Wave {
+        width: target_image_width,
+        height: target_image_height,
+        indices,
+    }
 }
